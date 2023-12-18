4d2bf5a9952f61c96bb9e76ce0227d31fa217f22
Brent Komer <brent.komer@gmail.com>
2023-12-18 17:53:14 -0500

    Day 18 - Part 2

--- 2023/day18.py
+++ 2023/day18.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy
+from collections import defaultdict
 
 dir_map = {
     "U": (-1, 0),
@@ -67,7 +68,7 @@ def decode(hex):
     return direction, int(distance, 16)
 
 
-def part2(fname="day18_input.txt"):
+def part2_first_pass(fname="day18_input.txt"):
     with open(fname, "r") as f:
         lines = f.readlines()
         loc = (0, 0)
@@ -122,6 +123,124 @@ def part2(fname="day18_input.txt"):
                         to_check.append(next_check)
 
         total -= len(edges.keys())
+
+        return total
+
+
+offsets = [
+    (1, 0),
+    (-1, 0),
+    (0, 1),
+    (0, -1),
+    (1, 1),
+    (1, -1),
+    (-1, 1),
+    (-1, -1),
+]
+
+
+def outline(point, edges):
+    for offset in offsets:
+        edges[(point[0] + offset[0], point[1] + offset[1])] = 1
+
+
+def part2(fname="day18_input.txt"):
+    with open(fname, "r") as f:
+        lines = f.readlines()
+        loc = (0, 0)
+        dug = {(0, 0): 1}
+        edges = {}
+        outline((0, 0), edges)
+        min_x = 0
+        min_y = 0
+        max_x = 0
+        max_y = 0
+        for line in lines:
+            d, n = decode(line[:-1].split(" ")[2])
+            n = int(n)
+
+            for i in range(n):
+                offset = dir_hex_map[d]
+                loc = (loc[0]+offset[0], loc[1]+offset[1])
+                dug[loc] = 1
+                outline(loc, edges)
+                min_x = min(min_x, loc[0])
+                max_x = max(max_x, loc[0])
+                min_y = min(min_y, loc[1])
+                max_y = max(max_y, loc[1])
+
+        # clear space from the edges
+        total = (max_x - min_x + 1) * (max_y - min_y + 1)
+
+        print("Dig complete")
+
+        # Follow the outer edge of the dugout shape
+
+        valid_edges = {}
+        # key is height, value is a list of vertical switch points
+        # for calculating area within bounds
+        left_edges = defaultdict(lambda: [])
+        right_edges = defaultdict(lambda: [])
+        to_check = []
+
+        for i in range(min_x, max_x + 1):
+            if (i, min_y) not in dug:
+                to_check.append((i, min_y))
+                valid_edges[(i, min_y)] = 1
+                left_edges[i].append(min_y)
+            if (i, max_y) not in dug:
+                to_check.append((i, max_y))
+                valid_edges[(i, max_y)] = 1
+                right_edges[i].append(max_y)
+
+        for i in range(min_y, max_y + 1):
+            if (min_x, i) not in dug:
+                to_check.append((min_x, i))
+        for i in range(min_y, max_y + 1):
+            if (max_x, i) not in dug:
+                to_check.append((max_x, i))
+
+        print("Edge check complete")
+
+        while len(to_check) > 0:
+            cur_check = to_check.pop()
+            for offset in dir_hex_map.values():
+                next_check = (cur_check[0] + offset[0], cur_check[1] + offset[1])
+                if next_check not in valid_edges and next_check in edges and next_check not in dug:
+                    if min_x <= next_check[0] <= max_x and min_y <= next_check[1] <= max_y:
+                        valid_edges[next_check] = 1
+                        left = (next_check[0], next_check[1] - 1)
+                        right = (next_check[0], next_check[1] + 1)
+                        if left in dug:
+                            left_edges[next_check[0]].append(next_check[1])
+                        if right in dug:
+                            right_edges[next_check[0]].append(next_check[1])
+
+                        to_check.append(next_check)
+
+        print("Valid edge computation complete")
+
+        for i in range(min_x, max_x + 1):
+            if i in left_edges:
+                left_list = sorted(left_edges[i])
+            else:
+                left_list = []
+            if i in right_edges:
+                right_list = sorted(right_edges[i])
+            else:
+                right_list = []
+
+            if len(left_list) == len(right_list):
+                for j in range(len(left_list)):
+                    total -= (right_list[j] - left_list[j] + 1)
+            elif len(left_list) > len(right_list):
+                for j in range(len(left_list)-1):
+                    total -= (right_list[j] - left_list[j] + 1)
+                total -= (max_y - left_list[-1] + 1)
+            else:
+                for j in range(len(left_list)):
+                    total -= (right_list[j+1] - left_list[j] + 1)
+                total -= (right_list[0] - min_y + 1)
 
         return total
 
