f91fbe962af8edd64e5de65f7e71d57ed6ebbea9
Brent Komer <brent.komer@gmail.com>
2023-12-23 11:55:18 -0500

    Day 23 - Part 2

--- 2023/day23.py
+++ 2023/day23.py
@@ -1,5 +1,6 @@
 import numpy as np
 from functools import lru_cache
+from collections import defaultdict
 import scipy
 
 offsets = (
@@ -16,15 +17,12 @@ def block(line, index):
 
 @lru_cache()
 def path_to_end(start, end, cur_map):
-    # if cur_map[start[0]][start[1]] == "#":
-    #     return -np.inf
     if start == end:
         return 0
     next_vals = []
     for offset in offsets:
         next_node = (start[0] + offset[0], start[1] + offset[1])
         if 0 <= next_node[0] < len(cur_map) and 0 <= next_node[1] < len(cur_map[0]):
-            # print(next_node)
             if cur_map[next_node[0]][next_node[1]] == "#":
                 continue
             elif cur_map[next_node[0]][next_node[1]] == ">" and offset != (0, 1):
@@ -62,18 +60,12 @@ def path_to_end(start, end, cur_map):
         return max(next_vals)
 
 
-def part1_old(fname="day23_input.txt"):
+def part1_recursive(fname="day23_input.txt"):
     with open(fname, "r") as f:
         lines = f.readlines()
-        arr = np.zeros((len(lines), len(lines[0])-1))
-        steppable = set()
-        tried = set()
         trail = tuple([line[:-1] for line in lines])
         start = (0, 1)
         end = (len(lines)-1, len(lines[0])-3)
-        # for i, line in enumerate(lines):
-        #     for j, char in enumerate(line[:-1]):
-        #         pass
 
         # keep trying paths, find the longest to the end
         return path_to_end(start, end, trail)
@@ -132,7 +124,7 @@ def path_to_end(cur_map, cur, end):
     return labels[cur[0], cur[1]] == labels[end[0], end[1]]
 
 
-def part2(fname="day23_input.txt"):
+def part2_slow(fname="day23_input.txt"):
     with open(fname, "r") as f:
         lines = f.readlines()
         trail = tuple([line[:-1] for line in lines])
@@ -165,7 +157,127 @@ def part2(fname="day23_input.txt"):
 
     return longest
 
-# print(part1(fname="day23_test.txt"))
-# print(part1(fname="day23_input.txt"))
-# print(part2(fname="day23_test.txt"))
+
+def get_longest_path(start, end, cur_map):
+    longest = 0
+
+    to_expand = [(start, cur_map, 0)]
+    while len(to_expand) > 0:
+        cur_node, cur_map, cur_step = to_expand.pop()
+        if cur_node == end:
+            longest = max(longest, cur_step)
+            continue
+        for offset in offsets:
+            next_node = (cur_node[0] + offset[0], cur_node[1] + offset[1])
+            if 0 <= next_node[0] < len(cur_map) and 0 <= next_node[1] < len(cur_map[0]):
+                if cur_map[next_node[0]][next_node[1]] == "#":
+                    continue
+                if path_to_end(cur_map, next_node, end):
+                    next_map = []
+                    for i, line in enumerate(cur_map):
+                        if i == next_node[0]:
+                            next_map.append(block(line, next_node[1]))
+                        else:
+                            next_map.append(line)
+                    next_map = tuple(next_map)
+                    to_expand.append((next_node, next_map, cur_step + 1))
+
+    return longest
+
+
+def opposite(node):
+    return -node[0], -node[1]
+
+
+def part2(fname="day23_input.txt"):
+    with open(fname, "r") as f:
+        lines = f.readlines()
+        trail = tuple([line[:-1] for line in lines])
+        start = (0, 1)
+        end = (len(lines)-1, len(lines[0])-3)
+
+    # find all of the junction points
+    tried = set()
+    junctions = set()
+    junctions.add(start)
+    junctions.add(end)
+    to_expand = [(start, start, (1, 0), (1, 0))]
+    connections = {}
+
+    while len(to_expand) > 0:
+        cur_node, last_junction, last_move, initial_junction_move = to_expand.pop()
+        if cur_node == end:
+            connections[(last_junction, end)] = (initial_junction_move, last_move)
+            continue
+        next_nodes = []
+        valid_offsets = []
+        for offset in offsets:
+            next_node = (cur_node[0] + offset[0], cur_node[1] + offset[1])
+            if 0 <= next_node[0] < len(trail) and 0 <= next_node[1] < len(trail[0]):
+                if trail[next_node[0]][next_node[1]] == "#":
+                    continue
+                else:
+                    next_nodes.append(next_node)
+                    valid_offsets.append(offset)
+        is_junction = len(next_nodes) >= 3
+        if is_junction:
+            connections[(last_junction, cur_node)] = (initial_junction_move, last_move)
+            connections[(cur_node, last_junction)] = (opposite(last_move), opposite(initial_junction_move))
+            junctions.add(cur_node)
+        else:
+            tried.add(cur_node)
+
+        for i, next_node in enumerate(next_nodes):
+            if (next_node in junctions or next_node not in tried) and valid_offsets[i] != opposite(last_move):
+                if is_junction:
+                    to_expand.append((next_node, cur_node, valid_offsets[i], valid_offsets[i]))
+                else:
+                    to_expand.append((next_node, last_junction, valid_offsets[i], initial_junction_move))
+
+    longest_paths = {}
+    neighbours = defaultdict(lambda: set())
+    for pair, directions in connections.items():
+        p_start = pair[0]
+        p_end = pair[1]
+        init_move = directions[0]
+
+        next_map = []
+        for i, line in enumerate(trail):
+            if i == p_start[0]:
+                next_map.append(block(line, p_start[1]))
+            else:
+                next_map.append(line)
+
+        lp = 1 + get_longest_path(
+            (p_start[0] + init_move[0], p_start[1] + init_move[1]),
+            p_end,
+            tuple(next_map)
+        )
+        longest_paths[pair] = lp
+
+        neighbours[p_start].add((p_end, lp))
+        neighbours[p_end].add((p_start, lp))
+
+    longest = 0
+
+    to_expand = [(start, 0, set(start))]
+
+    while len(to_expand) > 0:
+        cur_node, distance, cur_tried = to_expand.pop()
+        if cur_node == end:
+            longest = max(longest, distance)
+            continue
+        else:
+            for neighbour in neighbours[cur_node]:
+                if neighbour[0] not in cur_tried:
+                    next_tried = cur_tried.copy()
+                    next_tried.add(neighbour[0])
+                    to_expand.append((neighbour[0], distance + neighbour[1], next_tried))
+
+    return longest
+
+
+print(part1(fname="day23_test.txt"))
+print(part1(fname="day23_input.txt"))
+print(part2(fname="day23_test.txt"))
 print(part2(fname="day23_input.txt"))
